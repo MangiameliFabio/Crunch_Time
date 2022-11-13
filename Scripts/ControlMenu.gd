588e1c9d593919dfc754965ea27c71ffa7ac1071
Oli Klein <zwietabak@gmail.com>
2022-11-13 19:21:10 +0100

    Added control scene

---
+++ Scripts/ControlMenu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+onready var controls = $VBoxContainer as VBoxContainer
+onready var back_button = $BackButton as Button
+
+func _ready():
+	back_button.grab_focus()
+
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://Scene/MainMenu.tscn")
